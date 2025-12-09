073de0d51e2b6e58dcb2bef15b084d97f2b20a2c
Leandro Ferreira <leandro.ferreira@gmail.com>
2025-12-09 14:20:51 -0300

    docker desktop

--- o11yInstrumentation/install_otel_collector_local.sh
+++
@@ -1,4 +0,0 @@
-
-
-kubectl create ns splunk-o11y
-helm install splunk-otel-collector -n splunk-o11y --set="splunkObservability.accessToken=oeHSPfE2bUgGaIazsL7sTg,clusterName=digibank-k8s-docker,splunkObservability.realm=us1,gateway.enabled=false,splunkObservability.profilingEnabled=true,environment=digibank_lab,operatorcrds.install=true,operator.enabled=true,agent.discovery.enabled=true" splunk-otel-collector-chart/splunk-otel-collector
